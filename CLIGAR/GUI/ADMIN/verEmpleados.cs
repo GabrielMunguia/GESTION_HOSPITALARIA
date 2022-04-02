94c727feeae8c07c4707f406d3709e171cef9e39
Gabriel Munguia <juayua2011@gmail.com>
2022-04-01 19:39:04 -0600

    Se agregaron vistas modulo empleado

---
+++ CLIGAR/GUI/ADMIN/verEmpleados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLIGAR.GUI.ADMIN
+{
+    public partial class verEmpleados : Form
+    {
+        public verEmpleados()
+        {
+            InitializeComponent();
+        }
+    }
+}
